24c1f46f32526c2c86571b0dd66e3ff6e04c7e7e
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-07-21 21:18:41 +0930

    7- Testing the Authentication

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const { User } = require("../models/user");
 const mongoose = require("mongoose");
 const express = require("express");
+const Joi = require("joi");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
@@ -10,7 +11,7 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("Invalid email or password.");
+  if (!user) return res.status(400).send("Invalid email or password.");
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
 
