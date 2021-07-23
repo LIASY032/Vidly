63974c28c1b693979097792330542f786ac686ce
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-07-24 00:08:33 +0930

    11- Extracting Routes, 12- Extracting the Db Logic,  13- Extracting the Logging Logic

--- index.js
+++ index.js
@@ -1,75 +1,18 @@
-require("express-async-errors");
-require("winston-mongodb");
-const winston = require("winston");
-const mongoose = require("mongoose");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
 const config = require("config");
-const error = require("./middleware/error");
 
-// process.on("uncaughtException", (ex) => {
-//   winston.error(ex.message, ex);
-//   process.exit(1);
-// });
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/logging")();
 
-process.on("unhandledRejection", (ex) => {
-  throw ex;
-  // winston.error(ex.message, ex);
-  // process.exit(1);
-});
-
-winston.exceptions.handle(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-// I had the same issue, what I did was to replace this statement:
-
-// winston.add(winston.transports.File, { filename: 'logfile.log' });
-// to this:
-
-// winston.add(new winston.transports.File({ filename: 'logfile.log' }));
-// This happens in the latest major update of winston i.e 3.x.x and above.
-
-// Hope this helps!
-
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://localhost/vidly",
-    level: "info",
-  })
-);
-
-const p = Promise.reject(new Error("Something failed miserably!"));
-p.then(() => console.log("Done"));
+// const p = Promise.reject(new Error("Something failed miserably!"));
+// p.then(() => console.log("Done"));
 
 if (!config.get("jwtPrivateKey")) {
   console.log("FATAL ERROR: jwtPrivateKey is not defined");
   process.exit(1);
 }
-mongoose
-  .connect("mongodb://localhost/vidly", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB..."));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ startup/db.js
@@ -0,0 +1,14 @@
+const winston = require("winston");
+const mongoose = require("mongoose");
+
+module.exports = function () {
+  mongoose
+    .connect("mongodb://localhost/vidly", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => winston.info("Connected to MongoDB..."))
+    .catch((err) => console.error("Could not connect to MongoDB..."));
+};
